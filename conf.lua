3004f2aba1080844e416483b5313b74170d1a35a
Nibbl-z <thisisnotnibblz123@gmail.com>
2025-12-25 17:35:28 -0700

    feat: :tada: and thus it begins

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.window.depth = 16
+    t.window.title = "Goose Platformer 3D"
+end
---
+++ main.lua
@@ -0,0 +1,13 @@
+local g3d = require("g3d")
+
+function love.load()
+
+end
+
+function love.update()
+    
+end
+
+function love.draw()
+    
+end
